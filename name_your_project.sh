f6aef1170ae863d31f07cc9dd29b3b774aa7732d
Hao Jia <pascal.hjia@gmail.com>
2019-11-05 19:11:57 -0800

    changed file names to project_name*

--- name_your_project.sh
+++ name_your_project.sh
@@ -1,2 +1,5 @@
 
 find . -not -path '*/\.*' -name '*' -type f |xargs sed -i -e "s/project_name/$1/g"
+mv src/project_name.cpp src/$1.cpp
+mv include/project_name.h include/$1.h
+mv tests/project_name_unit.cpp tests/$1_unit.cpp
