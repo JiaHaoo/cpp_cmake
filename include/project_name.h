1c796e5a19fa09fea53fe8fff4b75db607173463
Hao Jia <pascal.hjia@gmail.com>
2019-11-27 17:15:11 -0800

    remove struct vec_str

--- include/project_name.h
+++ include/project_name.h
@@ -1,30 +1,10 @@
 #pragma once
 
-#include <cinttypes>
-#include <string>
-
 
 /**
- *  A vector of strings
- *  elements are stored in [begin, end)
- *  available space is [begin, limit)
+ * struct project_name
  */
 struct project_name
 {
-private:
-        std::string * begin;
-
-        std::string * end;
-        std::string * limit;
-
-public:
-        project_name() : begin(nullptr), end(nullptr), limit(nullptr) {}
-
-        project_name(size_t s);
-
-        uint64_t size() const {
-                return end - begin;
-        }
 
 };
-
